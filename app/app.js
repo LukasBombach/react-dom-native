696cac4cd7d7e5a814de3741abcccffb933a871c
Lukas Bombach <lukas.bombach@googlemail.com>
2021-09-29 20:19:05 +0200

    skia safe example

--- app/app.js
+++
@@ -1,7 +0,0 @@
-export function openWindow() {
-  return Deno.core.opSync("open_window"); // todo opAsync
-}
-
-export function closeWindow(rid) {
-  return Deno.core.opSync("close_window", rid);
-}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,147 @@
-pub mod app_handler;
-pub mod js;
-pub mod window;
-
-use app_handler::AppEvent;
-use app_handler::AppHandler;
-
-use std::sync::mpsc::channel;
-use std::thread;
-
-use glutin::event::Event;
-use glutin::event_loop::ControlFlow;
-use glutin::event_loop::EventLoop;
+use gl::types::*;
+use glutin::event::{Event, KeyboardInput, VirtualKeyCode, WindowEvent};
+use glutin::event_loop::{ControlFlow, EventLoop};
 use glutin::window::WindowBuilder;
+use glutin::GlProfile;
+use skia_safe::gpu::gl::FramebufferInfo;
+use skia_safe::gpu::{BackendRenderTarget, SurfaceOrigin};
+use skia_safe::{Color, ColorType, Surface};
+use std::convert::TryInto;
+
+mod renderer;
+
+type WindowedContext = glutin::ContextWrapper<glutin::PossiblyCurrent, glutin::window::Window>;
 
 fn main() {
-    let event_loop = EventLoop::<AppEvent>::with_user_event();
-    let event_loop_proxy = event_loop.create_proxy();
-    let (send, recv) = channel();
-    let app_handler = AppHandler::new(event_loop_proxy, recv);
+    let el = EventLoop::new();
+    let wb = WindowBuilder::new().with_title("rust-skia-gl-window");
 
-    thread::spawn(|| {
-        let mut js_runtime = js::Runtime::new();
-        js_runtime.run("app/index.js", app_handler);
-    });
+    let cb = glutin::ContextBuilder::new()
+        .with_depth_buffer(0)
+        .with_stencil_buffer(8)
+        .with_pixel_format(24, 8)
+        .with_double_buffer(Some(true))
+        .with_gl_profile(GlProfile::Core);
 
-    event_loop.run(move |event, event_loop, control_flow| {
+    let windowed_context = cb.build_windowed(wb, &el).unwrap();
+
+    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+    let pixel_format = windowed_context.get_pixel_format();
+
+    println!(
+        "Pixel format of the window's GL context: {:?}",
+        pixel_format
+    );
+
+    gl::load_with(|s| windowed_context.get_proc_address(s));
+
+    let mut gr_context = skia_safe::gpu::DirectContext::new_gl(None, None).unwrap();
+
+    let fb_info = {
+        let mut fboid: GLint = 0;
+        unsafe { gl::GetIntegerv(gl::FRAMEBUFFER_BINDING, &mut fboid) };
+
+        FramebufferInfo {
+            fboid: fboid.try_into().unwrap(),
+            format: skia_safe::gpu::gl::Format::RGBA8.into(),
+        }
+    };
+
+    windowed_context
+        .window()
+        .set_inner_size(glutin::dpi::Size::new(glutin::dpi::LogicalSize::new(
+            1024.0, 1024.0,
+        )));
+
+    fn create_surface(
+        windowed_context: &WindowedContext,
+        fb_info: &FramebufferInfo,
+        gr_context: &mut skia_safe::gpu::DirectContext,
+    ) -> skia_safe::Surface {
+        let pixel_format = windowed_context.get_pixel_format();
+        let size = windowed_context.window().inner_size();
+        let backend_render_target = BackendRenderTarget::new_gl(
+            (
+                size.width.try_into().unwrap(),
+                size.height.try_into().unwrap(),
+            ),
+            pixel_format.multisampling.map(|s| s.try_into().unwrap()),
+            pixel_format.stencil_bits.try_into().unwrap(),
+            *fb_info,
+        );
+        Surface::from_backend_render_target(
+            gr_context,
+            &backend_render_target,
+            SurfaceOrigin::BottomLeft,
+            ColorType::RGBA8888,
+            None,
+            None,
+        )
+        .unwrap()
+    }
+
+    let surface = create_surface(&windowed_context, &fb_info, &mut gr_context);
+    // let sf = windowed_context.window().scale_factor() as f32;
+    // surface.canvas().scale((sf, sf));
+
+    let mut frame = 0;
+
+    // Guarantee the drop order inside the FnMut closure. `WindowedContext` _must_ be dropped after
+    // `DirectContext`.
+    //
+    // https://github.com/rust-skia/rust-skia/issues/476
+    struct Env {
+        surface: Surface,
+        gr_context: skia_safe::gpu::DirectContext,
+        windowed_context: WindowedContext,
+    }
+
+    let mut env = Env {
+        surface,
+        gr_context,
+        windowed_context,
+    };
+
+    el.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
+        #[allow(deprecated)]
         match event {
-            Event::UserEvent(AppEvent::CreateWindow) => {
-                let window = WindowBuilder::new().build(&event_loop).unwrap();
-                send.send(window).unwrap();
+            Event::LoopDestroyed => {}
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::Resized(physical_size) => {
+                    env.surface =
+                        create_surface(&env.windowed_context, &fb_info, &mut env.gr_context);
+                    env.windowed_context.resize(physical_size)
+                }
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            virtual_keycode,
+                            modifiers,
+                            ..
+                        },
+                    ..
+                } => {
+                    if modifiers.logo() {
+                        if let Some(VirtualKeyCode::Q) = virtual_keycode {
+                            *control_flow = ControlFlow::Exit;
+                        }
+                    }
+                    frame += 1;
+                    env.windowed_context.window().request_redraw();
+                }
+                _ => (),
+            },
+            Event::RedrawRequested(_) => {
+                {
+                    let canvas = env.surface.canvas();
+                    canvas.clear(Color::WHITE);
+                    renderer::render_frame(frame % 360, 12, 60, canvas);
+                }
+                env.surface.canvas().flush();
+                env.windowed_context.swap_buffers().unwrap();
             }
             _ => (),
         }
